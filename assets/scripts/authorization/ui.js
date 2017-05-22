7715db6469257fe0354708534d1ef20393bbe13b
mlynnes <micaelalsassano@gmail.com>
2017-05-22 11:44:02 -0400

    Adds bare bones UI file
    The next step will be adding console.logs for testing
    and different conditions for success and failures for
    user authorization.
    
    Also adds module exports for all needed conditions.

---
+++ assets/scripts/authorization/ui.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const store = require('../store.js')
+
+const signUpSuccess = (data) => {
+  
+}
+
+const signUpFailure = () => {
+
+}
+
+const signInSuccess = () => {
+
+}
+
+const signInFailure = () => {
+
+}
+
+const changePasswordSuccess = (response) => {
+
+}
+
+const changePasswordFailure = () => {
+
+}
+
+const signOutSuccess = () => {
+
+}
+
+const signOutFailure = () => {
+
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
