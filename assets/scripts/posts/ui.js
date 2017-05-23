28d366f6a2ca8d40c66f53b5362d92bbb723ab5d
mlynnes <micaelalsassano@gmail.com>
2017-05-23 12:24:55 -0400

    Adds conditions to UI
    Only have added console logs and errors for testing our CRUD
    actions. Have added conditions for success and failure of all actions.

---
+++ assets/scripts/posts/ui.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const createPostSuccess = (response) => {
+  console.log('created post and response is:', response)
+}
+
+const createPostFailure = (error) => {
+  console.error(error)
+}
+
+const getPostsSuccess = (response) => {
+  console.log('gets post and response is:', response)
+}
+
+const getPostsFailure = (error) => {
+  console.error(error)
+}
+
+const updatePostSucess = (response) => {
+  console.log('updates post and response is:', response)
+}
+
+const updatePostFailure = (error) => {
+  console.error(error)
+}
+
+const deletePostSuccess = (response) => {
+  console.log('deletes post and response is:', response)
+}
+
+const deletePostFailure = (error) => {
+  console.error(error)
+}
+
+module.exports = {
+  createPostSuccess,
+  createPostFailure,
+  getPostsSuccess,
+  getPostsFailure,
+  updatePostSucess,
+  updatePostFailure,
+  deletePostSuccess,
+  deletePostFailure
+}
