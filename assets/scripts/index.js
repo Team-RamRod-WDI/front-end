33ae89d27a4deed974479b700908e6b9bb02aee7
David Henriquez <David@Davids-MacBook-Pro.local>
2017-05-23 15:43:17 -0400

    Adds get posts and delete posts
    Creates necessary API language and events to get posts and delete a
    user's posts.
    
    Need to now change the initial api to get posts just for a particular
    page and user

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,10 +3,12 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const authEvents = require('./authorization/events.js')
+const postEvents = require('./posts/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
   authEvents.addHandlers()
+  postEvents.addHandlers()
 })
 
 // use require with a reference to bundle the file and use it in this file
